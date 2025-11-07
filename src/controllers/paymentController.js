d990c5ad37a331cc80f319a608b30ad29f21661f
palmero-jerome07 <jeromepalmero7@gmail.com>
2025-11-07 15:38:29 +0800

    changes

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,9 +1,34 @@
 import Payment from "../models/paymentModel.js";
 
 const paymentController = {
+  getAll: async (req, res) => {
+    try {
+      const payments = await Payment.getAll();
+      res.status(200).json({
+        success: true,
+        customers: payments,
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: "Error fetching customers",
+        error: err.message,
+      });
+    }
+  },
+
   create: async (req, res) => {
     try {
       const { order_id, amount_paid, payment_mode } = req.body;
+
+      if (!order_id || !amount_paid || !payment_mode) {
+        return res.status(400).json({
+          success: false,
+          message:
+            "All fields (order_id, amount_paid, payment_mode) are required",
+        });
+      }
+
       const payment = await Payment.create({
         order_id,
         amount_paid,
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import paymentController from "../controllers/paymentController.js";
 const router = express.Router();
 
+router.get("/", paymentController.getAll);
 router.post("/", paymentController.create);
 
 export default router;
